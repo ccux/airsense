378be30c24b167cb263900e51960ce35fa1c74f8
ccux <Christian.chr@gmail.com>
2017-05-08 11:45:01 +0200

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,8 +1,8 @@
-//Set the loading indicator
+/*//Set the loading indicator
   var loading = document.getElementById("loading");
   //console.log("Div is: ", loadingDiv);
   loading.style.display = "block"
-
+*/
 
 var rooms = [];
 var greenColorMin = 0;
@@ -412,9 +412,10 @@ setAirQualityGraphs();
 
 
 $(window).load(function() {
-  
+ /* 
   var loadingDiv = document.getElementById("loading");
   //console.log("Div is: ", loadingDiv);
   loadingDiv.style.display = "none"
  // $("#loading").fadeOut();
+ */
 });
