679b0901ce19fb1857f3624a745ba1dcc4a12fa9
ccux <Christian.chr@gmail.com>
2017-08-10 22:52:53 +0200

    Add files via upload

--- mantis.js
+++ mantis.js
@@ -418,6 +418,24 @@ roomTitle.innerHTML = sensorData.sensors[i].location.toUpperCase();//rooms[i].na
 var temperature = document.getElementsByClassName('temperature-number')[i];
 temperature.innerHTML = Math.round(sensorData.sensors[i].latest_data.value);//rooms[i].temp;
 
+
+//HTML ROOM BLOCK
+var roomBlockHTML = '<div class="room-block"><a class="room-detail-link w-inline-block" ';
+roomBlockHTML += 'href="http://mantis-e9c0de.webflow.io/detail?roomid=';
+roomBlockHTML += i;
+roomBlockHTML += '" id="Room-1-Block"></a><div class="room-block-content"><h4 class="room-block-title" id="demo-title">';
+roomBlockHTML += sensorData.sensors[i].location.toUpperCase();
+roomBlockHTML += '</h4><img class="room-block-icon" height="50" src=';
+roomBlockHTML += '"http://uploads.webflow.com/58dab8fd2bebde920b1f3557/58db8014b2a7d646468737e8_Room_Square_Block_Icon.png"';
+roomBlockHTML += ' width="50"><h1 class="room-block-temperature temperature-number">';
+roomBlockHTML += temperature;
+roomBlockHTML += '</h1><h1 class="degree-symbol room-block-temperature">∘</h1><div class="room-block-humitity-row w-row"><div class="column-3 w-col w-col-6"><div class="room-block-data-title">Humidity</div></div><div class="column-4 w-col w-col-6"><h4 class="room-block-humidity">';
+roomBlockHTML += '64';
+roomBlockHTML += '%</h4></div></div><div class="room-block-air-quality-row room-block-humitity-row w-row"><div class="column-3 w-col w-col-6"><div class="airquality room-block-data-title">Air Quality</div></div><div class="column-4 w-col w-col-6"><img class="room-block-airquality-status-image" height="21" src="http://uploads.webflow.com/58dab8fd2bebde920b1f3557/58db7da10b307284023739b1_1-full.svg" width="21"></div></div></div></div>';
+
+
+var roomBlockDiv = document.getElementsByClassName('room-container').innerHTML = roomBlockHTML;
+
 //Set Humidity color
 /*var humidity = document.getElementsByClassName('room-block-humidity')[i];
 humidity.innerHTML = rooms[i].humidity + "%";
