97d4b91c4963e7c77235e01e991d8d0c57c264be
ccux <Christian.chr@gmail.com>
2017-08-13 23:09:45 +0200

    Add files via upload

--- detail.js
+++ detail.js
@@ -593,11 +593,11 @@ buildResult += '<div class="graph-row w-row"><div class="column-9 w-col w-col-10
 /*onObject*///document.getElementsByClassName("graph-week-colum-bar-container")[1].innerHTML = 
 
 $('#temperature-indicator-5').html("<p>" + Math.round(maxTemp) + "</p>");
-$('#temperature-indicator-5').html("<p>" + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 +"</p>");
-$('#temperature-indicator-5').html("<p>" + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 +"</p>");
-$('#temperature-indicator-5').html("<p>" + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 +"</p>");
-$('#temperature-indicator-5').html("<p>" + Math.round(maxTemp) / 5 +"</p>");
-$('#temperature-indicator-5').html("<p>" + 0 + "</p>");
+$('#temperature-indicator-4').html("<p>" + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 +"</p>");
+$('#temperature-indicator-3').html("<p>" + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 +"</p>");
+$('#temperature-indicator-2').html("<p>" + Math.round(maxTemp) / 5 + Math.round(maxTemp) / 5 +"</p>");
+$('#temperature-indicator-1').html("<p>" + Math.round(maxTemp) / 5 +"</p>");
+$('#temperature-indicator-0').html("<p>" + 0 + "</p>");
 
 $(".graph-week-colum-bar-container").eq(1).html(buildResult);
 
