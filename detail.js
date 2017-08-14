09cc077d543efe5091b682d69711703a5315894f
ccux <Christian.chr@gmail.com>
2017-08-14 21:57:04 +0200

    Add files via upload

--- detail.js
+++ detail.js
@@ -651,8 +651,8 @@ console.log("Max temp is:" + Math.round(maxTemp));
 
 //Show the correstonding graph
 
-
-var graphBarScale = 145 / (Math.ceil((Math.round(maxTemp)+1)/10)*10);
+var roundedMaxValue = (Math.ceil((Math.round(maxTemp)+1)/10)*10);
+var graphBarScale = 145 / roundedMaxValue;
 
 //Reverse data array
 averageDataSet.reverse();
@@ -678,17 +678,17 @@ $('#temperature-indicator-2').html(Math.round((maxTemp / 5) * 2));
 $('#temperature-indicator-1').html(Math.round(maxTemp / 5));
 $('#temperature-indicator-0').html(0);*/
 
-$('#temperature-indicator-5').html(Math.ceil((Math.round(maxTemp)+1)/10)*10);
+$('#temperature-indicator-5').html(roundedMaxValue);
 //$('#temperature-indicator-4').html(Math.ceil((Math.round((maxTemp / 5) * 4)+1)/10)*10);
 //$('#temperature-indicator-3').html(Math.ceil((Math.round((maxTemp / 5) * 3)+1)/10)*10);
 //$('#temperature-indicator-2').html(Math.ceil((Math.round((maxTemp / 5) * 2)+1)/10)*10);
 //$('#temperature-indicator-1').html(Math.ceil((Math.round((maxTemp / 5) * 1)+1)/10)*10);
 $('#temperature-indicator-0').html(0);
 
-$('#temperature-indicator-4').html((Math.ceil(((Math.round((maxTemp) * 1)+1)/10)*10) / 5) * 4);
-$('#temperature-indicator-3').html((Math.ceil(((Math.round((maxTemp) * 1)+1)/10)*10) / 5) * 3);
-$('#temperature-indicator-2').html((Math.ceil(((Math.round((maxTemp) * 1)+1)/10)*10) / 5) * 2);
-$('#temperature-indicator-1').html((Math.ceil(((Math.round((maxTemp) * 1)+1)/10)*10) / 5) * 1);
+$('#temperature-indicator-4').html(Math.round((roundedMaxValue / 5) * 4));
+$('#temperature-indicator-3').html(Math.round((roundedMaxValue / 5) * 3));
+$('#temperature-indicator-2').html(Math.round((roundedMaxValue / 5) * 2));
+$('#temperature-indicator-1').html(Math.round((roundedMaxValue / 5) * 1));
 
 
 $(".graph-week-colum-bar-container").eq(1).html(buildResult);
