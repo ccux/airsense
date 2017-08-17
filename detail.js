529bbd9f3a2eb5905a90d842b8690ac48f57d6e2
ccux <Christian.chr@gmail.com>
2017-08-17 22:31:28 +0200

    Add files via upload

--- detail.js
+++ detail.js
@@ -952,10 +952,10 @@ $('#humidity-indicator-0').html(0);
 //Add the build results to the DOM
 $(".graph-week-colum-bar-container").eq(0).html(buildResult);
 
-var criticalValueBarPlacement = ((141 / maxTemp) * 60) + 52 - 24;
-var minCriticalValueBarPlacement = ((141 / maxTemp) * 40) + 52 - 24;
-$(".humidity-graph-red-bar").css({ top: -criticalValueBarPlacement + 'px' });
-$(".humidity-graph-red-bar.yellow-bar").css({ top: -minCriticalValueBarPlacement + 'px' });
+var criticalValueBarPlacement = ((141 / maxTemp) * 60) + 52;
+var minCriticalValueBarPlacement = ((141 / maxTemp) * 40) + 52;
+$(".humidity-graph-red-bar").css({ bottom: criticalValueBarPlacement + 'px' });
+$(".humidity-graph-red-bar.yellow-bar").css({ bottom: minCriticalValueBarPlacement + 'px' });
 
 //Display the temperature data
 $(".history-section").eq(0).fadeIn(500);
