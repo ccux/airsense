5432124761cd015fe90c26702dee5a979758a08d
ccux <Christian.chr@gmail.com>
2017-08-18 12:05:51 +0200

    Add files via upload

--- mantis.js
+++ mantis.js
@@ -521,15 +521,77 @@ else {
 
 function webSocket () {
 
-  var socket = new WebSocket("ws://172.104.145.165/ws/1?subscribe-broadcast");
+      //The user has WebSockets
 
+      connect();
+
+      function connect(){
+          var socket;
+          var host = "ws://172.104.145.165/ws/1?subscribe-broadcast";
+
+          try{
+              var socket = new WebSocket(host);
+
+              message('<p class="event">Socket Status: '+socket.readyState);
+
+              socket.onopen = function(){
+               message('<p class="event">Socket Status: '+socket.readyState+' (open)');
+              }
+
+              socket.onmessage = function(msg){
+               message('<p class="message">Received: '+msg.data);
+              }
+
+              socket.onclose = function(){
+                message('<p class="event">Socket Status: '+socket.readyState+' (Closed)');
+              }     
+
+          } catch(exception){
+             message('<p>Error'+exception);
+          }
+
+          function send(){
+              var text = $('#text').val();
+
+              if(text==""){
+                  message('<p class="warning">Please enter a message');
+                  return ;
+              }
+              try{
+                  socket.send(text);
+                  message('<p class="event">Sent: '+text)
+
+              } catch(exception){
+                 message('<p class="warning">');
+              }
+              $('#text').val("");
+          }
+
+          function message(msg){
+            $('#chatLog').append(msg+'</p>');
+          }
+
+          $('#text').keypress(function(event) {
+              if (event.keyCode == '13') {
+                send();
+              }
+          }); 
+
+          $('#disconnect').click(function(){
+             socket.close();
+          });
+
+      }//End connect
+
+ /* 
+   var socket = new WebSocket("ws://172.104.145.165/ws/1?subscribe-broadcast");
   socket.onopen = function(){
   console.log("Socket has been opened!");
   }
 
   socket.onmessage = function(msg){
   console.log(msg); //Awesome!
-  }
+  }*/
 
 
 /*
