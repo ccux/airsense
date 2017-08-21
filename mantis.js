25d940ca315cae7fa05b4a5fb8476dc91b5879b7
ccux <Christian.chr@gmail.com>
2017-08-21 14:07:37 +0200

    Add files via upload

--- mantis.js
+++ mantis.js
@@ -571,8 +571,9 @@ function updateWithData (data) {
 
 	//Update DOM temperature
 	var roomTemperature = $('#room-temperature-' + createSensorID(parsedData.mac_address));
+	roomTemperature.fadeOut(function() {
 	roomTemperature.text(Math.round(parsedData.data[2].value)).fadeIn(500);
-	
+	});
 	//Update the DOM humidity
 	var roomHumidity = $('#room-humidity-' + createSensorID(parsedData.mac_address));
 	roomHumidity.text(Math.round(parsedData.data[1].value) + '%').fadeIn(500);
