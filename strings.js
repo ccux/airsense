948b3006c22dbd58119211b9a64af27b197e17fb
ccux <Christian.chr@gmail.com>
2017-08-21 23:39:30 +0200

    Add files via upload

--- strings.js
+++ strings.js
@@ -2,8 +2,9 @@
 
 var userLang = navigator.language || navigator.userLanguage; 
 
-var strings {
+var strings = {
 	
+	//Front page
 	hello: {
 	en: 'Hello',
 	da: 'Hej'
@@ -22,6 +23,36 @@ var strings {
 	airQuality: {
 	en: 'Air Quality',
 	da: 'Luftkvalitet'
-	}
+	},
 
+	credits: {
+	en: 'AirSense is developed by Xtel Wireless Denmark',
+	da: 'AirSense er udviklet af Xtel Wireless'
+	},
+
+	//Details
+	status: {
+	en: 'STATUS',
+	da: 'STATUS'
+	},
+
+	possible_improvement: {
+	en: 'POSSIBLE IMPROVEMENT',
+	da: 'MULIG FORBEDRING'
+	},
+
+	graph_day: {
+	en: 'DAY',
+	da: 'DAG'
+	},
+
+	graph_week: {
+	en: 'WEEK',
+	da: 'UGE'
+	},
+
+	graph_now: {
+	en: 'NOW',
+	da: 'NU'
+	}
 };
